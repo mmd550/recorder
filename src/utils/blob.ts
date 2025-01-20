21ec869dc11482e0bbbc4ce306bd28a3b2db0205
Mohammad Farvardin <mmd77frvrdn@gmail.com>
2025-01-20 10:42:47 +0330

    fix: Make sure that the chunks received from media recorder will be removed by gaurbage collector

--- src/utils/blob.ts
+++ src/utils/blob.ts
@@ -1,22 +1,35 @@
+let linkElement: HTMLAnchorElement | undefined
+
+const getLinkEl = () => {
+  if (linkElement) return linkElement
+
+  const a = document.createElement('a') as HTMLAnchorElement
+  a.style.display = 'none'
+  document.body.appendChild(a)
+  linkElement = a
+  return linkElement
+}
+
 export function saveFile(blobList: Blob[], type: string, name = 'untitled') {
   return new Promise((resolve, reject) => {
     if (!('URL' in window))
-      reject(new Error('Unable to save the file. Browser doesn`t support window.URL'))
+      reject(
+        new Error(
+          'Unable to save the file. Browser doesn`t support window.URL',
+        ),
+      )
     else {
-      const blob = new Blob(blobList, { type })
+      let blob: Blob | undefined = new Blob(blobList, { type })
       const url = window.URL.createObjectURL(blob)
 
-      const a = document.createElement('a')
-      a.style.display = 'none'
+      const a = getLinkEl()
       a.href = url
       a.download = name
-      document.body.appendChild(a)
       a.click()
 
       setTimeout(() => {
         if ('URL' in window) window.URL.revokeObjectURL(url)
-
-        document.body.removeChild(a)
+        blob = undefined
         resolve('File saved successfully')
       }, 100)
     }
